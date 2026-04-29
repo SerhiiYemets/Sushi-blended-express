aa9af2fb6bb21d7adc8f802dd45d0e748ae10648
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-04-29 12:22:02 +0200

    fix logger for production

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,15 +1,22 @@
 import pino from 'pino-http';
 
-export const logger = pino({
-  level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-      messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      hideObject: true,
-    },
-  },
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const logger = pino(
+  isProduction
+    ? {
+        level: 'info',
+      }
+    : {
+        level: 'info',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss',
+            ignore: 'pid,hostname',
+            messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+          },
+        },
+      }
+);
